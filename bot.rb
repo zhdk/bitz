d2911bb5137c0d2c5a4ca21680c3fbd596901ce1
Ramón Cahenzli <rca@psy-q.ch>
2013-04-18 15:22:54 +0200

    Initial commit.

---
+++ bot.rb
@@ -0,0 +1,112 @@
+require 'cinch'
+
+class Hello
+  include Cinch::Plugin
+
+  match "hello"
+
+  def execute(m)
+    m.reply "Hello, #{m.user.nick}"
+  end
+end
+
+
+class Urls
+  include Cinch::Plugin
+
+  listen_to :channel, :method => :url_responder
+  listen_to :private, :method => :url_responder
+
+  def url_responder(m)
+    if m.message =~ /!url (.+)/
+      process_url(m)
+    end
+  end
+
+  def process_url(m)
+    matches = m.message.match(/!url (.+)/)
+    string = matches[1]
+
+    # TODO: Stick all these URLs in a hash, then respond accordingly if the url message matches 
+    #       one of the hash keys. Maybe make it a general-purpose info dictionary?
+    if m.channel == "#leihs"
+      if string == "docs"
+        m.reply "The leihs documentation is at http://github.com/zhdk/leihs/wiki"
+      elsif string == "blog"
+        m.reply "The leihs blog is at http://blog.zhdk.ch/leihs"
+      elsif string == "group"
+        m.reply "The leihs Google group is at http://groups.google.com/group/leihs"
+      else
+        m.reply "I don't have the faintest idea where or what '#{string}' could be"
+      end
+    end
+
+    if m.channel == "#madek"
+      if string == "docs"
+        m.reply "The Madek documentation is at http://github.com/zhdk/madek/wiki"
+      else
+        m.reply "I don't have the faintest idea where or what '#{string}' could be"
+      end
+    end
+
+  end
+end
+
+class Help
+  include Cinch::Plugin
+  listen_to :channel, :method => :help_responder
+
+  def help_responder(m)
+    if m.message == "!help"
+      m.user.send("Hi there! Try one of the following commands:")
+      m.user.send("!url docs    | Shows the URL for the project's documentation")
+      m.user.send("!url blog    | Shows the URL for the project's blog")
+      m.user.send("!url group   | Shows our Google group's URL")
+      m.user.send("!latest      | Checks for the latest available versions of our software")
+    end
+  end
+end
+
+class Release
+  include Cinch::Plugin
+
+  listen_to :channel, :method => :release_responder
+
+  def release_responder(m)
+    if m.message == "!latest"
+      if m.channel == "#leihs"
+        latest_30 = `git ls-remote -t http://github.com/zhdk/leihs.git | cut -f 3 -d "/" | grep -v \"\\^\" | sort -V | tail -n 1`.strip
+        latest_29 = `git ls-remote -t http://github.com/zhdk/leihs.git | cut -f 3 -d "/" | grep \"^2\.\" | grep -v \"\\^\" | sort -V | tail -n 1`.strip
+        if latest_30.empty? or latest_29.empty?
+          sorry(m)
+        else
+          m.reply("The latest version of leihs on GitHub is #{latest_29} for the 2.9 branch and #{latest_30} for the 3.x branch.")
+        end
+      elsif m.channel == "#madek"
+        latest = `git ls-remote -t http://github.com/zhdk/madek.git | cut -f 3 -d "/" | grep -v \"\\^\" | sort -V | tail -n 1`
+        latest.strip!
+        if latest.empty?
+          sorry(m)
+        else
+          m.reply("The latest version of Madek on GitHub is #{latest}.")
+        end
+      end
+    end
+  end
+
+  def sorry(m)
+    m.reply("Sorry, the latest release can't be determined at the moment. Please try again later.")
+  end
+end
+
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick = "bitzbot"
+    c.server = "irc.freenode.org"
+    c.channels = ["#leihs", "#madek"]
+    c.plugins.plugins = [Urls, Release, Help]
+  end
+end
+
+bot.start
